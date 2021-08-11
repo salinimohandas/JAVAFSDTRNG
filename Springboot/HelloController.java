942ca2c9f8e96b1286f107ca6275566748030eec
salinimohandas <87758077+salinimohandas@users.noreply.github.com>
2021-08-11 16:22:57 +0530

    commit changas

---
+++ Springboot/HelloController.java
@@ -0,0 +1,17 @@
+package com.ust.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "This is java training";
+	}
+
+}
