6771b6256a2debe2d5c9bbd9e60d78c23bebc34b
salinimohandas <87758077+salinimohandas@users.noreply.github.com>
2021-09-06 09:59:03 +0530

    commit changes

---
+++ AngularSPA/demo1/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Employee {
+	@Id
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ "]";
+	}
+	
+}
